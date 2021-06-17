672dfb7b6a9bffb062e069f10967c8ca5e36698f
Felix Deiters <messismore@users.noreply.github.com>
2021-06-17 23:47:14 +0200

    Hack for image paths with special characters
    Stupid hack to get Nuxt/Image to work with paths with special characters in them. If something image related breaks, look here first.

--- plugins/rehype-use-nuxt-picture.js
+++ plugins/rehype-use-nuxt-picture.js
@@ -9,6 +9,14 @@ module.exports = (options) => (tree) => {
     (node) => node.tagName === 'img',
     (node) => {
       node.tagName = 'nuxt-picture'
+      /*
+      Nuxt/Content does the right thing and URI-encodes the paths of our
+      assets. But now Nuxt/Image won't find our source files anymore.
+      This is probably a terrible idea that will come back to bite us, but for
+      now, let's simply re-decode our sanitised paths. Too bad we're stubborn
+      like that.
+      */
+      node.properties.src = decodeURI(node.properties.src)
     }
   )
 }
